8824f416debca82225068b31dd08f9f67acca5d6
malbrecht <m.albrecht@neusta.de>
2019-09-20 13:38:32 +0200

    Issue-#4999 : fixed mark brick as NOT dirty instead of "last $key"

--- models/DataObject/Objectbrick/Dao.php
+++ models/DataObject/Objectbrick/Dao.php
@@ -118,7 +118,7 @@ class Dao extends Model\DataObject\Fieldcollection\Dao
                 $setter = 'set' . ucfirst($type);
 
                 if ($brick instanceof DataObject\DirtyIndicatorInterface) {
-                    $brick->markFieldDirty($key, false);
+                    $brick->markFieldDirty('_self', false);
                 }
 
                 $this->model->$setter($brick);
